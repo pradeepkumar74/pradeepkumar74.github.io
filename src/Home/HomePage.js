d84ad3beaffa632552ea3f21bdb916409fdfc512
pradeepkumar74 <pradeep.kur2010@gmail.com>
2024-03-01 21:41:27 -0800

    added home page text

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 function HomePageComponent(){
-return <div>Welcome to Home page.</div>
+return <div>Welcome to my first application Home page.</div>
 };
 export default HomePageComponent;
